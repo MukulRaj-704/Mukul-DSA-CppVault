ed1525ed824b8ae4a333250e50cebf48b7634f27
Mukul Raj <mukulraj9829@gmail.com>
2025-08-05 16:27:46 +0530

    Update and rename 74. Search a 2D Matrix to 74. Search a 2D Matrix.cpp

---
+++ Binary-Search/74. Search a 2D Matrix.cpp
@@ -0,0 +1,50 @@
+###################################################################
+OPTIMAL APPROACH 
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+            int r = matrix.size();
+            int c  = matrix[0].size();
+            int low =0, high = (r*c)-1;
+            while(low<=high){
+                int mid = (low+high)/2;
+                int i=mid/c,j=mid%c;
+                if(matrix[i][j]==target){
+                    return true;
+                }
+                if(matrix[i][j]>=target){
+                    high = mid-1;
+                }
+                else{
+                    low= mid+1;
+                }
+            }
+
+    return false;}
+};
+
+
+########################################################################
+BETTER APPROACH
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+            int r = matrix.size();
+            int c  = matrix[0].size();
+            int low =0, high = (r*c)-1;
+            while(low<=high){
+                int mid = (low+high)/2;
+                int i=mid/c,j=mid%c;
+                if(matrix[i][j]==target){
+                    return true;
+                }
+                if(matrix[i][j]>=target){
+                    high = mid-1;
+                }
+                else{
+                    low= mid+1;
+                }
+            }
+
+    return false;}
+};
