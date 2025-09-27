1a131cc6e049e01675d5deb222ca077c7f1db9a4
Mukul Raj <mukulraj9829@gmail.com>
2025-09-27 22:44:07 +0530

    Create Previous Greater Element.c

---
+++ Stack/Previous Greater Element.c
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> preGreaterEle(vector<int>& arr) {
+        //  code here
+        int n = arr.size();
+        stack<int> st;
+        vector<int> res(n,-1);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && st.top()<=arr[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                res[i]=st.top();
+            }
+            st.push(arr[i]);
+        }
+        return res;
+    }
+};
