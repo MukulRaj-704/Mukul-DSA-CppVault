cc8f9104c537d7883afe796d7a96c9dce4e652a7
Mukul Raj <mukulraj9829@gmail.com>
2025-10-18 21:33:11 +0530

    Update 110. Balanced Binary Tree.cpp

--- Tree/110. Balanced Binary Tree.cpp
+++ Tree/110. Balanced Binary Tree.cpp
@@ -29,3 +29,35 @@ public:
         return 1+max(lh,rh);
     }
 };
+
+
+BACKTRACKING 
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL){
+            return true;
+        }
+        int maxlen=0;
+        int count =1;
+        return helper(root,count,maxlen)!=-1;
+        
+    }
+    private:
+    int helper(TreeNode* root, int &count, int &maxlen){
+        if(root==NULL) return 0;
+        maxlen=max(maxlen,count);
+        count++;
+        int lh=helper(root->left,count,maxlen);
+        int rh= helper(root->right, count,maxlen);
+        count--;
+        if(lh==-1 || rh==-1){
+            return -1;
+        }
+        if(abs(lh-rh)>1){
+            return -1;
+        }
+        return 1+max(lh,rh);
+    }
+};
