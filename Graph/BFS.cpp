ead9be705432e3fe07a871be5c04c3157ff61922
Mukul Raj <mukulraj9829@gmail.com>
2025-12-10 19:28:45 +0530

    Implement BFS algorithm in BFS.cpp

---
+++ Graph/BFS.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // code here
+        int n =  adj.size();
+        //int visited[n]={0};
+        vector<int> visited(n,0);
+        queue<int>q;
+        q.push(0);
+        visited[0]=1;
+        vector<int> ans;
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node]){
+                if(visited[it]==0){
+                    visited[it]=1;
+                    q.push(it);
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
