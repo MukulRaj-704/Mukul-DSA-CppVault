a5b04af96f314efce94908c9d14504b7e4370290
Mukul Raj <mukulraj9829@gmail.com>
2025-12-30 23:31:31 +0530

    Create 53 Maximum Subarray.cpp

---
+++ Two-Pointer/53 Maximum Subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        
+        int maxsum=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            
+            sum=sum+nums[i];
+            maxsum = max(maxsum,sum);
+            if(sum<0){
+                sum=0;
+            }
+            
+        }
+        return maxsum;
+    }
+};
