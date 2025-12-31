3e17421ea98c56f6e528f31830717c90c8760578
Mukul Raj <mukulraj9829@gmail.com>
2025-12-31 20:02:10 +0530

    Create Ceil of Integer X.cpp

---
+++ BST/Ceil of Integer X.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int findCeil(Node* root, int x) {
+        // code here
+        int ans=-1;
+     while(root!=nullptr){
+         if(root->data==x) return root->data;
+         else if(root->data<x) root= root->right;
+         else{
+             ans=root->data;
+             root=root->left;
+             
+         }
+     }
+     return ans;
+    }
+};
