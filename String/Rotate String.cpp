39e114efcd98e4557d2aa98800c61efd1f51b24e
Mukul Raj <mukulraj9829@gmail.com>
2026-01-19 17:31:26 +0530

    Update Rotate String.cpp

--- String/Rotate String.cpp
+++ String/Rotate String.cpp
@@ -11,3 +11,25 @@ public:
         return false;
     }
 };
+
+
+// Approach---2
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n=s.size();
+        int m=goal.size();
+        string org=s;
+        if(n!=m) return false;
+        for(int i=0;i<n;i++){
+            s=org;
+            reverse(s.begin(),s.begin()+i);
+            reverse(s.begin()+i,s.end());
+             reverse(s.begin(),s.end());
+            if(s==goal) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
